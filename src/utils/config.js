220575e96e591e3bd5f51fa3ea01d6dc51d5a363
yangdoubleming <yangdoubleming@163.com>
2020-03-11 11:17:20 +0800

    上传config文件

---
+++ src/utils/config.js
@@ -0,0 +1,9 @@
+// const BASE_URL='http://test.api.dowsure.com/freightsure'  //接口地址
+// const IMG_URL='http://test.api.dowsure.com/freightsure'  // 图片地址
+const BASE_URL='http://139.196.29.173:8081'  //刘畅
+// const BASE_URL='http://192.168.10.130:8081'  //李丰仁
+// const BASE_URL='http://api.dowsure.com/freightsure'  //接口地址
+const IMG_URL='http://test.static.dowsure.com'  // 图片地址
+const IMG_URL_show='http://test.static.dowsure.com'  // 图片显示地址
+const DETAILS_URL='http://api.dowsure.com/data'  // 详情单独地址
+export{ BASE_URL, IMG_URL, IMG_URL_show, DETAILS_URL}
